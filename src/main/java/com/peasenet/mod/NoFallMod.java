f49cd9e44352b301e27cd0b4e6aeef30fd91c06c
Gavin <gavinpease@gmail.com>
2023-07-21 12:25:10 -0600

    Remove logging, and optionally removed mixins file if required

--- src/main/java/com/peasenet/mod/NoFallMod.java
+++ src/main/java/com/peasenet/mod/NoFallMod.java
@@ -7,14 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoFallMod implements ModInitializer {
-    public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-        LOGGER.info("Initializing GEM NoFall");
         GavinsMod.addMod(new ModNoFall());
     }
 }
